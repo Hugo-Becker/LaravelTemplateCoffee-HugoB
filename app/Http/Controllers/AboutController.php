0cebb604c7b57e510e098005229810f4df3d8f35
Hugo <becker.hugo@hotmail.com>
2021-01-31 17:13:27 +0100

    asy stout

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -7,8 +7,15 @@ use Illuminate\Support\Facades\Route;
 
 class AboutController extends Controller
 {
+    
     public function about(){
-        $currentRoute=Route::getFacadeRoot()->current()->uri();;
-        return view('pages.about',compact('currentRoute'));
+        $about=array(
+
+            $titre1=(object)['titre1'=>'Strong Coffee, Strong Roots',"titre2"=>"About Our Cafe","paragraph"=>'<p>Founded in 1987 by the Hernandez brothers, our establishment has been serving up rich coffee sourced from artisan farmers in various regions of South and Central America. We are dedicated to travelling the world, finding the best coffee, and bringing back to you here in our cafe.</p>
+            <p class="mb-0">We guarantee that you will fall in <em>lust</em> with our decadent blends the moment you walk inside until you finish your last sip. Join us for your daily routine, an outing with friends, or simply just to enjoy some alone time.</p>']
+
+        );
+        $currentRoute=Route::getFacadeRoot()->current()->uri();
+        return view('pages.about',compact('currentRoute','about'));
     }
 }
--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,9 +8,20 @@ use Illuminate\Support\Facades\Route;
 class ProductsController extends Controller
 {
     public function products(){
+        $products=(object)[
+            "titre1"=>"Blended to Perfection",
+            "titre2"=>"Coffees & Teas",
+            "paragraph1"=>"We take pride in our work, and it shows. Every time you order a beverage from us, we guarantee that it will be an experience worth having. Whether it's our world famous Venezuelan Cappuccino, a refreshing iced herbal tea, or something as simple as a cup of speciality sourced black coffee, you will be coming back for more.",
+            "titre3"=>"Delicious Treats, Good Eats",
+            "titre4"=>"Bakery & Kitchen",
+            "paragraph2"=>"Our seasonal menu features delicious snacks, baked goods, and even full meals perfect for breakfast or lunchtime. We source our ingredients from local, oragnic farms whenever possible, alongside premium vendors for specialty goods.",
+            "titre5"=>"Oups je l'ai delete",
+            "titre6"=>"pareil",
+            "paragraph3"=>"Travelling the world for the very best quality coffee is something take pride in. When you visit us, you'll always find new blends from around the world, mainly from regions in Central and South America. We sell our blends in smaller to large bulk quantities. Please visit us in person for more details."
+        ];
         $currentRoute=Route::getFacadeRoot()->current()->uri();;
 
-        return view('pages.products',compact('currentRoute'));
+        return view('pages.products',compact('currentRoute','products'));
     }
     //
 }
